2b90ce88390ad0cbbf0e0e866b7b00d650f58f00
Jose Daniel Hernandez Osorio <josedaniel.hernandez.osorio@bbva.com>
2023-03-26 21:03:50 -0600

    create docker-compose postgres

--- backend_imatpro_system/settings.py
+++ backend_imatpro_system/settings.py
@@ -16,14 +16,16 @@ import environ
 from pathlib import Path
 
 # environment
-
 env = environ.Env(
+    # set casting, default value
     DEBUG=(bool, False)
 )
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
