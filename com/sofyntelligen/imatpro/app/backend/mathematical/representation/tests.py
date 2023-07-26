2246dbd854e386bc0e44dd56d2c5b67f0b37fe16
Jose Daniel Hernandez Osorio <josedaniel.hernandez.osorio@bbva.com>
2023-07-25 19:31:41 -0600

    add test api equation

--- com/sofyntelligen/imatpro/app/backend/mathematical/representation/tests.py
+++ com/sofyntelligen/imatpro/app/backend/mathematical/representation/tests.py
@@ -7,7 +7,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 
-class CharacterTestCase(TestCase):
+class RepresentationTestCase(TestCase):
     fixtures = ['catalog', 'character', 'equation', 'representation']
 
     def test_search_character_all(self):
